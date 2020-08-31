d5dccde342e756ed68fe5147771d35218a3c391f
Fahad <fahadshihab@gmail.com>
2020-08-31 22:51:52 +0530

    asd

--- team_login.php
+++ team_login.php
@@ -3,8 +3,8 @@ $servername = "localhost";
 $username = "maveli";
 $password = "maveli";
 $dbname = "ONAMHUNT";
-/*
 $conn = new mysqli($servername, $username, $password, $dbname);
+/*
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
