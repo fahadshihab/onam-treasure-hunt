bbaaab6e2a172fcafb2bbc9c6c90b438432c3f2a
Fahad <fahadshihab@gmail.com>
2020-08-31 23:30:58 +0530

    xzv

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
                     <div class="col col-12 col-md-4 text-center">
                         <?php
                             if($_GET["auth"] == "fail"){
-                                echo '<span class="text-danger"> auth fail! </span>'
+                                echo '<span class="text-danger"> auth fail! </span>';
                             }
                         ?>
                         <form method="GET" action="team_login.php">
