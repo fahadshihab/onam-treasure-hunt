b9b1d4f90dd7f58b50a6990d8cb335e99589d310
Fahad <fahadshihab@gmail.com>
2020-08-31 23:19:18 +0530

    asd

--- team_login.php
+++ team_login.php
@@ -7,10 +7,10 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if (!$conn) {
 	echo "failed";
 }
-$team_name = $_REQUEST["team_name"];
-$team_code = $_REQUEST["team_code"];
-$validator = /^[A-Za-z0-9]+$/;
+$team_name = $_GET["team_name"];
+$team_code = $_GET["team_code"];
 /*
+$validator = /^[A-Za-z0-9]+$/;
 if (preg_match($validator, $team_name) && preg_match($validator, $team_code)){
     $sql = "SELECT * FROM team_details WHERE team_name = '" . $team_name . "' AND team_code = '" . $team_code . "'";
     $result = mysqli_query($conn, $sql);
