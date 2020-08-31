c5e9d205dfc2ce14961bb58eeed71baba1d6df70
Fahad <fahadshihab@gmail.com>
2020-08-31 22:53:06 +0530

    asd

--- team_login.php
+++ team_login.php
@@ -4,13 +4,13 @@ $username = "maveli";
 $password = "maveli";
 $dbname = "ONAMHUNT";
 $conn = new mysqli($servername, $username, $password, $dbname);
-/*
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 $team_name = $_REQUEST["team_name"];
 $team_code = $_REQUEST["team_code"];
 $validator = /^[A-Za-z0-9]+$/;
+/*
 if (preg_match($validator, $team_name) && preg_match($validator, $team_code)){
     $sql = "SELECT * FROM team_details WHERE team_name = '" . $team_name . "' AND team_code = '" . $team_code . "'";
     $result = mysqli_query($conn, $sql);
