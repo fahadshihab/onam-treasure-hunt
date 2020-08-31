5b55901419928ab7b51c322d27a945828d9d7734
Fahad <fahadshihab@gmail.com>
2020-08-31 11:26:09 +0530

    ssf

--- js/login.js
+++ js/login.js
@@ -16,6 +16,6 @@ function login(name, code){
             window.location.replace("https://parakkampaachil.heromanhua.com/game.html");
         }
     };
-    xhr.open("GET", "../php/team_login.php?team_name=" + name +"&team_code=" + code, false);
+    xhr.open("GET", "../php/team_login.php?team_name=" + name +"&team_code=" + code, true);
     xhr.send();
 }
