10e72a51b3188b34b19e41529c802e814bc9cc0b
Fahad <fahadshihab@gmail.com>
2020-08-31 22:13:20 +0530

    dv

--- index.php
+++ index.php
@@ -58,31 +58,7 @@
             </div>
         </nav>
     </section>
-    <?php
-        if (isset($_GET["submit"])){
-            $servername = "localhost";
-            $username = "maveli";
-            $password = "maveli";
-            $dbname = "ONAMHUNT";
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-            }
-            $team_name = $_GET["team_name"];
-            $team_code = $_GET["team_code"];
-            $validator = /^[A-Za-z0-9]+$/;
-            if (preg_match($validator, $team_name) && preg_match($validator, $team_code)){
-                $sql = "SELECT * FROM team_details WHERE team_name = '" . $team_name . "' AND team_code = '" . $team_code . "'";
-                $result = mysqli_query($conn, $sql);
-                if (mysqli_num_rows($result) > 0) {
-                    header("Location: game.php");
-                } else {
-                    header("Location: index.php");
-                }
-            }
-            mysqli_close($conn);
-        }
-    ?>
+    
 
     <section id="home">
         <div class="hero">
