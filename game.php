ac831c043549e5383840373c0bcb6cf04fc58b12
Fahad <fahadshihab@gmail.com>
2020-09-01 14:16:19 +0530

    vz

--- game.php
+++ game.php
@@ -63,44 +63,46 @@
                     }else if($_GET["attempt" == "fail"]){
                         echo '<span class="text-danger">ഉത്തരം തെറ്റാണ്</span>';
                     }else{
-                        echo '<span class="text-info">ഇതിനു ശരിയുത്തരം നേരത്തെ തന്നതാണ്.</span>';
+                        echo '<span class="text-success">ഇതിനു ശരിയുത്തരം നേരത്തെ തന്നതാണ്.</span>';
                     }
                 }
                 ?>
                 <div class="space-20"></div>
-                <?php
-                $servername = "localhost";
-                $username = "maveli";
-                $password = "maveli";
-                $dbname = "ONAMHUNT";
-                $problem_register = array(
-                    "problem1",
-                    "problem2",
-                    "problem3",
-                    "problem4",
-                    "problem5",
-                    "problem6",
-                    "problem7"
-                );
-                $conn = new mysqli($servername, $username, $password, $dbname);
-                if (!$conn) {
-                    echo "failed";
-                }
-                $sql = 'SELECT * FROM scoreboard WHERE team_code = "' . $_SESSION["team_code"] . '"';
-                $result = mysqli_query($conn, $sql);
-                if(mysqli_num_rows($query) > 0){
-                    $row = mysqli_fetch_assoc($result);
-                    for($i = 0; $i < 7; $i++){
-                        echo readfile("../onam/" . $problem_register[$i] . ".txt");
-                        if(empty($row[$problem_register[$i]])){
-                            break;
-                        }
+                <div class="accordion">
+                    <?php
+                    $servername = "localhost";
+                    $username = "maveli";
+                    $password = "maveli";
+                    $dbname = "ONAMHUNT";
+                    $problem_register = array(
+                        "problem1",
+                        "problem2",
+                        "problem3",
+                        "problem4",
+                        "problem5",
+                        "problem6",
+                        "problem7"
+                    );
+                    $conn = new mysqli($servername, $username, $password, $dbname);
+                    if (!$conn) {
+                        echo "failed";
                     }
-                }else{
-                    echo "<span>onnum kitteela</span>";
-                }
-                mysqli_close();
-                ?>
+                    $sql = 'SELECT * FROM scoreboard WHERE team_code = "' . $_SESSION["team_code"] . '"';
+                    $result = mysqli_query($conn, $sql);
+                    if(mysqli_num_rows($result) > 0){
+                        $row = mysqli_fetch_assoc($result);
+                        for($i = 0; $i < 7; $i++){
+                            echo readfile("../onam/" . $problem_register[$i] . ".txt");
+                            if(empty($row[$problem_register[$i]])){
+                                break;
+                            }
+                        }
+                    }else{
+                        echo "<span>onnum kitteela</span>";
+                    }
+                    mysqli_close();
+                    ?>
+                </div>
             </div>
         </div>
     </section>
