d7074294dd4d8f36c00da8dfd2366f0b423b40a3
Fahad <fahadshihab@gmail.com>
2020-09-02 14:59:22 +0530

    sdf

--- scores.php
+++ scores.php
@@ -114,14 +114,12 @@
                         $result = mysqli_query($conn, $sql);
                         if(mysqli_num_rows($result) > 0){
                             while($row = mysqli_fetch_assoc($result)){
-                                for($i=0; $i<1; $i++){
+                                for($i=0; $i<7; $i++){
                                     $date[$i] = 0;
-                                    /*
                                     if($row[$problem_register[$i]] != NULL){
                                         $date[$i] = strtotime($row[$problem_register[$i]]);
                                         $date[$i] += 19800;
                                     }
-                                    */
                                 }
                                 echo "
                                 <tr>
