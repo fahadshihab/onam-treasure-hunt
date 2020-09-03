66a33c5f1caa7b86706f6cc70ad20fb3ed522fb4
Fahad <fahadshihab@gmail.com>
2020-09-03 19:22:00 +0530

    zc

--- game.php
+++ game.php
@@ -27,7 +27,6 @@
 </head>
 <body class="bg-dark2 text-white" data-target="#nav" data-spy="scroll" data-offset="500">
     <section>
-        <!-- Here goes navbar -->
         <nav id="nav" class="navbar navbar-expand-md navbar-dark bg-dark2 fixed-top">
             <a class="navbar-brand smooth-goto text-white" href="#home">
                 പരക്കംപാച്ചിൽ
@@ -68,6 +67,40 @@
             <div class="space-20"></div>
             <div class="space-20"></div>
             <div class="row">
+            <div class="col col-sm-12">
+            <span id="demo"></span>
+
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 3, 2020 23:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = "Countdown: " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Cowntdown: FINISHED!";
+  }
+}, 1000);
+</script>
+            </div>
                 <div class="col col-sm-12">
                 <?php 
                 if(isset($_GET["attempt"])){
